ddcffbe109aaf104ebcc7398ca550c4ae38eb52a
Nico Linder <nl@fp.de>
2019-10-09 14:03:32 +0200

    refactor tests and extend them

--- tests/TypedPersistence.CouchbaseLite/FSharp.Tests/Tests.fs
+++ tests/TypedPersistence.CouchbaseLite/FSharp.Tests/Tests.fs
@@ -1,6 +1,5 @@
 namespace TypedPersistence.CouchbaseLite.FSharp.Tests
 
-open Couchbase.Lite
 open FsCheck
 open FsCheck.Xunit
 open FsUnit
@@ -8,86 +7,39 @@ open TypedPersistence.CouchbaseLite.FSharp
 open Xunit
 
 module ``Saving and loading tests`` =
-    type GenericRecord<'a> = { value: 'a }
-    type GenericRecord2<'a, 'b> = { value1: 'a; value2: 'b }
-
     let documentName = "testdoc"
 
-    let openDatabase () =
-        new Database("testdb")
+    let saveToDatabase<'a> = saveToDatabase<'a> documentName
+    let loadFromDatabase<'a> () = loadFromDatabase<'a> documentName
+    let simplePropertyTest<'a when 'a : equality> = simplePropertyTest<'a> documentName
+    let genericPropertyTest<'a when 'a : equality> = genericPropertyTest<'a> documentName
 
-    let cleanUpDatabase () =
-        use db = openDatabase ()
-        db.Delete()
-
-    let saveToDatabase data =
-        use db = openDatabase ()
-        saveDocument db documentName data
-        db.Close ()
-
-    let loadFromDatabase<'a> () =
-        use db = openDatabase ()
-        let result = loadDocument<'a> db documentName
-        db.Close()
-        result
-
-    let genericPropertyTest<'a when 'a : equality> setUp (data: 'a) =
-        let dataRecord = { GenericRecord.value = data }
-
-        cleanUpDatabase ()
-
-        setUp dataRecord
-
-        saveToDatabase dataRecord
-
-        let result = loadFromDatabase<GenericRecord<'a>> ()
-
-        match result with
-        | Ok record ->
-            if record.value = data then
-                true
-            else
-                printfn "Expected: %A - Got: %A" data record.value
-                false
-        | Error error ->
-            printfn "Error: %A" error
-            false
-
-    let simplePropertyTest<'a when 'a : equality> (data: 'a) =
-        genericPropertyTest ignore data
-
-    (*[<Fact>]
+    [<Fact>]
     let ``Fails to load unexisting document`` () =
         cleanUpDatabase ()
 
         use db = openDatabase ()
-        let result = loadDocument<int> db documentName
+        let result = loadFromDatabase<int> ()
         db.Close()
 
-        result |> should equal (Error DocumentNotExisting)
+        result |> categorize
+        |> should equal DocumentNotExistingErrorCase
 
     [<Fact>]
     let ``Fails to load unexisting property`` () =
         cleanUpDatabase ()
 
-        use db = openDatabase ()
-        saveDocument db documentName { value = 0 }
-        db.Close ()
+        saveToDatabase { value1 = 0 }
+        let result = loadFromDatabase<GenericRecord<int>> ()
 
-        use db = openDatabase ()
-        let result = loadDocument<GenericRecord<int>> db documentName
-        db.Close()
-
-        let error = ValueNotExisting "value" |> Error
-        result |> should equal error*)
+        result |> categorize
+        |> should equal ValueNotExistingErrorCase
 
     [<Property>]
-    let ``Handles ints correctly`` (number: int) =
-        simplePropertyTest<int> number
+    let ``Handles ints correctly`` (number: int) = simplePropertyTest<int> number
 
     [<Property>]
-    let ``Handles double saving correctly`` (number: int) =
-        genericPropertyTest<int> saveToDatabase number
+    let ``Handles double saving correctly`` (number: int) = genericPropertyTest<int> saveToDatabase number
 
     [<Property>]
     let ``Handles strings correctly`` (text: NonNull<string>) =
@@ -105,12 +57,10 @@ module ``Saving and loading tests`` =
         simplePropertyTest<string option> text
 
     [<Property>]
-    let ``Handles option ints correctly`` (numberOption: Option<int>) =
-        simplePropertyTest<int option> numberOption
+    let ``Handles option ints correctly`` (numberOption: Option<int>) = simplePropertyTest<int option> numberOption
 
     [<Property>]
-    let ``Handles int list correctly`` (numberList: int list) =
-        simplePropertyTest<int list> numberList
+    let ``Handles int list correctly`` (numberList: int list) = simplePropertyTest<int list> numberList
 
     [<Property>]
     let ``Handles string list correctly`` (textList: NonNull<string> list) =
@@ -124,12 +74,13 @@ module ``Saving and loading tests`` =
 
     [<Property>]
     let ``Handles string record correctly`` (textRecord: GenericRecord<NonNull<string>>) =
-        { value = textRecord.value.Get }
-        |> simplePropertyTest<GenericRecord<string>>
+        { value = textRecord.value.Get } |> simplePropertyTest<GenericRecord<string>>
 
     [<Property>]
     let ``Handles int option, string record2 correctly`` (record: GenericRecord2<int option, NonNull<string>>) =
-        let record = { value1 = record.value1; value2 = record.value2.Get }
+        let record =
+            { value1 = record.value1
+              value2 = record.value2.Get }
 
         simplePropertyTest<GenericRecord2<int option, string>> record
 
