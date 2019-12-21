b17e78fd7d466a858f853ec3e492ddb6ef2be81e
Nico Linder <nico@nicoviii.net>
2019-12-21 14:45:09 +0100

    overhaul stuff

--- src/TypedPersistence/FSharp/Types.fs
+++ src/TypedPersistence/FSharp/Types.fs
@@ -9,20 +9,30 @@ module Types =
         inherit FSharpBsonMapper()
         let resolveCollectionName = this.ResolveCollectionName
 
-        do this.ResolveCollectionName <- Func<Type,string>(
-            fun t ->
-                if t.IsGenericType then
-                    t.FullName |> hash |> string
-                else
-                    resolveCollectionName.Invoke(t)
-        )
+        let rec genericName (t: Type) =
+            if t.IsGenericType then
+                t.GetGenericArguments()
+                |> List.ofArray
+                |> List.map genericName
+                |> List.reduce (fun a b -> a + "," + b)
+                |> (+) (t.Name + "-")
+                |> hash
+                |> string
+            else
+                resolveCollectionName.Invoke(t)
+
+        do
+            this.ResolveCollectionName <-
+                Func<Type, string>(fun t ->
+                    if t.IsGenericType then
+                        genericName t
+                    else
+                        resolveCollectionName.Invoke(t))
 
     type GenericEntry<'T> =
-        {
-            id: string
-            entry: 'T
-        }
+        { id: string
+          entry: 'T }
 
     type LoadError =
-    | DatabaseNotExisting
-    | DocumentNotExisting
+        | DatabaseNotExisting
+        | DocumentNotExisting
