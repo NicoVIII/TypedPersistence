87b5d5171a1940c94806f5e4b4cee22d23e74db0
NicoVIII <nico@nicoviii.net>
2020-09-12 14:39:01 +0000

    Introduce tests

--- src/TypedPersistence/Saving.fs
+++ src/TypedPersistence/Saving.fs
@@ -8,7 +8,14 @@ open System.IO
 module Saving =
     let saveVersion (filePath: string) (version: uint) (data: 'a) =
         // Alias for writing text with single arguments and not with tuple
-        let writeAllText filePath content = File.WriteAllText(filePath, content)
+        let writeAllText filePath content =
+            // Create path, if not existing
+            filePath
+            |> Path.GetDirectoryName
+            |> Directory.CreateDirectory
+            |> ignore
+            // Write data
+            File.WriteAllText(filePath, content)
 
         { version = version; data = data }
         |> Json.serialize
--- tests/TypedPersistence.UnitTests/Tests.fs
+++ tests/TypedPersistence.UnitTests/Tests.fs
@@ -1,17 +1,70 @@
 module Tests
 
 open Expecto
+open FsCheck
+open System.IO
+
 open TypedPersistence
 
+let testFileDir = "test-files"
+
+type BasicRecord<'a> = { data: 'a }
+
+/// Function to create a basic save and load test for a given type
+let basicSaveLoadTest<'a when 'a: equality> () =
+    let typeName = (typeof<'a>).FullName
+
+    let filepath =
+        "SaveLoad"
+        + typeName
+        |> System.Text.Encoding.ASCII.GetBytes
+        |> System.Security.Cryptography.MD5.Create().ComputeHash
+        |> Seq.map (fun c -> c.ToString("X2"))
+        |> Seq.reduce (+)
+        |> (fun name -> Path.Combine(testFileDir, name + ".test.json"))
+
+    testProperty ("Save and load: " + typeName) (fun (value: 'a) ->
+        save filepath value
+        let valueLoaded = (load<'a> filepath).Value
+        valueLoaded = value)
+
+/// Test list which is used by Expecto
 let tests =
     testList
-        "Group of tests"
-        [ test "Simple saving and loading" {
-              let test = "Hello World"
-              save "test.json" test
-              let test2 = load<string> "test.json"
-              Expect.equal test2.Value test "The strings should equal"
-          }
+        "Test List"
+        [ testList
+            "Save and load"
+              [ basicSaveLoadTest<NonNull<string>> ()
+                basicSaveLoadTest<int> ()
+                basicSaveLoadTest<NonNull<string> list> ()
+                basicSaveLoadTest<int list> ()
+                basicSaveLoadTest<BasicRecord<NonNull<string>>> ()
+                basicSaveLoadTest<BasicRecord<int>> ()
+                basicSaveLoadTest<BasicRecord<NonNull<string> list>> ()
+                basicSaveLoadTest<BasicRecord<int list>> ()
+                basicSaveLoadTest<BasicRecord<BasicRecord<int>>> () ]
+          testList
+              "Versioning"
+              [ testProperty "Load version" (fun (version: uint) ->
+                    let filepath =
+                        Path.Combine(testFileDir, "load_version.test.json")
 
-          testProperty "Reverse of reverse of a list is the original list" (fun (xs: list<int>) ->
-              List.rev (List.rev xs) = xs) ]
+                    saveVersion filepath version "Hello World"
+                    let versionLoaded = (getVersion filepath).Value
+                    versionLoaded = version)
+                test "Load by version" {
+                    let filepath =
+                        Path.Combine(testFileDir, "load_by_version.test.json")
+
+                    let data = { data = "Hello World" }
+
+                    saveVersion filepath 3u data
+
+                    let value =
+                        match getVersion filepath with
+                        | Some 3u -> (load<BasicRecord<string>> filepath).Value
+                        | Some _
+                        | None -> failwith "Could not load correct version!"
+
+                    Expect.equal value data "Data should be equal"
+                } ] ]
