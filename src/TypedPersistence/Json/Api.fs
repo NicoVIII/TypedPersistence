8a2cf21f9d3e7dab65054f6aa5acd3d80e81eddc
NicoVIII <nico@nicoviii.net>
2021-05-19 16:45:10 +0000

    Small adjustments

--- src/TypedPersistence/Json/Api.fs
+++ src/TypedPersistence/Json/Api.fs
@@ -1,13 +1,15 @@
 namespace TypedPersistence.Json
 
 open TypedPersistence.Core
+open TypedPersistence.Json.Types
 
 module Api =
     let fsharpProvider =
-        { new IPersistenceProvider<string> with
+        { new IPersistenceProvider<Context> with
             member _.getVersion context = getVersion context
             member _.load<'a> context = load<'a> context
             member _.save<'a> context object = save<'a> context object
+
             member _.saveVersion<'a> context version object =
                 saveVersion<'a> context version object }
 
