a52fcf659dde7975f192b531edd37303e48b1e2a
NicoVIII <nico@nicoviii.net>
2022-02-24 11:07:07 +0000

    Further modernization

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,7 +6,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
-## [0.6.0-alpha.7]
 ### Added
 - JSON support
 - Unified Provider classes for F# and C#
---
+++ run.fs
@@ -0,0 +1,105 @@
+open Fake.IO
+
+open RunHelpers
+open RunHelpers.BasicShortcuts
+open RunHelpers.Templates
+
+[<RequireQualifiedAccess>]
+module Config =
+    let projects =
+        [ "./src/TypedPersistence/Core/TypedPersistence.Core.fsproj"
+          "./src/TypedPersistence/Json/TypedPersistence.Json.fsproj" ]
+
+    let packDir = "./pack"
+
+    let testProject =
+        "./tests/TypedPersistence.UnitTests/TypedPersistence.UnitTests.fsproj"
+
+module Task =
+    let restore () =
+        job {
+            DotNet.toolRestore ()
+
+            for project in Config.projects do
+                DotNet.restore project
+
+            DotNet.restore Config.testProject
+        }
+
+    let build config =
+        job {
+            for project in Config.projects do
+                DotNet.build project config
+
+            DotNet.build Config.testProject config
+        }
+
+    let pack version =
+        job {
+            for project in Config.projects do
+                DotNet.pack Config.packDir project version
+        }
+
+    let test () = DotNet.run Config.testProject
+
+    [<RequireQualifiedAccess>]
+    module Docs =
+        let build () =
+            Shell.cp "./README.md" "docs/index.md"
+
+            dotnet [ "fsdocs"; "build"; "--clean" ]
+
+        let watch () =
+            Shell.deleteDir "./tmp"
+
+            Shell.cp "./README.md" "docs/index.md"
+
+            dotnet [ "fsdocs"; "watch" ]
+
+[<EntryPoint>]
+let main args =
+    args
+    |> List.ofArray
+    |> function
+        | [ "restore" ] -> Task.restore ()
+        | []
+        | [ "build" ] ->
+            job {
+                Task.restore ()
+                Task.build Debug
+            }
+        | [ "pack"; version ] ->
+            job {
+                Task.restore ()
+                Task.build Release
+                Task.pack version
+            }
+        | [ "test" ] ->
+            job {
+                Task.restore ()
+                Task.test ()
+            }
+        | [ "build-docs" ] ->
+            job {
+                Task.restore ()
+                Task.build Debug
+                Task.Docs.build ()
+            }
+        | [ "docs" ] ->
+            job {
+                Task.restore ()
+                Task.build Debug
+                Task.Docs.watch ()
+            }
+        // Missing args cases
+        | [ "pack" ] ->
+            let msg = [ "Usage: dotnet run pack <version>" ]
+            Error(1, msg)
+        // Default error case
+        | _ ->
+            let msg =
+                [ "Usage: dotnet run [<command>]"
+                  "Look up available commands in run.fs" ]
+
+            Error(1, msg)
+    |> ProcessResult.wrapUp
